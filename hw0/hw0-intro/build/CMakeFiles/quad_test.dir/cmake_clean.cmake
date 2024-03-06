4c4cd5387c4e32b6ce13e4b5b9138de4638b63c1
Kiriratanak Vong <kiriratanak.vong@gmail.com>
2024-03-06 14:53:31 -0800

    .

---
+++ hw0/hw0-intro/build/CMakeFiles/quad_test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/quad_test.dir/quad_test.cpp.o"
+  "CMakeFiles/quad_test.dir/quad_test.cpp.o.d"
+  "quad_test"
+  "quad_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/quad_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
